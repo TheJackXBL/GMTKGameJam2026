b0434d018b6ad3d4649b602e9d142dea5d4f5d88
LiviRose <livirosegames@gmail.com>
2026-07-22 22:53:01 -0500

    Base Streak Code

--- Raindrops/raindrop.gd
+++ Raindrops/raindrop.gd
@@ -3,21 +3,46 @@ extends RigidBody2D
 @onready var streak_container = get_parent().get_node("StreakContainer")
 
 var streak : Line2D
-var streak_minDistance := 2.0
+@export var streak_width := 8.0
+@export var streak_minDistance := 2.0
 
 var last_position : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	streak = Line2D.new()
-	streak.width = 8
+	
+	#Varying width code
+	streak.width = streak_width #TODO: streak.width = radius * variance * 1.5
+	var curve := Curve.new()
+	curve.add_point(Vector2(0.0, 1.0))
+	curve.add_point(Vector2(1.0, 0.2))
+	streak.width_curve = curve
+	
+	#Rounded Edges, future proofing for when they wiggle waggle
+	streak.begin_cap_mode = Line2D.LINE_CAP_ROUND
+	streak.end_cap_mode = Line2D.LINE_CAP_ROUND
+	streak.joint_mode = Line2D.LINE_JOINT_ROUND
+	
 	streak.default_color = Color.WHITE
+	#TODO: Replace above line with texture below
+#	streak.texture = preload("res://Textures/waterStreak.png")
+#	streak.texture_mode = Line2D.LINE_TEXTURE_TILE
+	
+	streak.modulate.a = 0.5
+	
+	#streak shader
+	var shader = load("res://Scenes/Car/waterStreak.gdshader")
+	streak.material = ShaderMaterial.new()
+	streak.material.shader = shader
+	
 	streak_container.add_child(streak)
 	
-	streak.add_point(
-		streak.to_local(global_position)
-	)
-	last_position = global_position
+	if global_position.distance_to(last_position) > streak_minDistance:
+		streak.add_point(
+			streak.to_local(global_position)
+		)
+		last_position = global_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
