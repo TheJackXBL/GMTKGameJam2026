c0d20aba2e4d474fc7592c11509f727b1aa137cc
LiviRose <livirosegames@gmail.com>
2026-07-25 02:44:33 -0500

    Added Score System

--- Scenes/Jack/race_manager.gd
+++ Scenes/Jack/race_manager.gd
@@ -4,6 +4,14 @@ extends Node2D
 
 @export var race_camera: Camera2D
 
+@onready var finishLine = $"../FinishLine"
+
+var score: int = 0
+var races_won: int = 0
+var races_played: int = 0
+
+signal score_changed(new_score)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,12 +22,15 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_start_race_button_pressed() -> void:
-	pass # Replace with function body.
+	races_played += 1
+	finishLine.reset_race()
 
 
 func _on_finish_line_winner_determined(winning_raindrop: Node2D) -> void:
 	
 	var winner_is_selected: bool = winning_raindrop.get("isSelected")
+	
+	update_score(true, winner_is_selected)
 	
 	if winner_is_selected:
 		print("The selected raindrop won!")
@@ -40,3 +51,17 @@ func _on_finish_line_winner_determined(winning_raindrop: Node2D) -> void:
 	RaindropManager.fade_remaining_raindrops()
 	
 	RaindropManager.prepare_and_spawn_raindrops()
+
+func update_score(playedRace: bool, won: bool) -> void:
+	if playedRace:
+		if won:
+			score += 100
+			races_won += 1
+			print("Winner! +100 points")
+		else:
+			print("Lost race. No points.")
+	else:
+		pass #TODO: Shop logic
+	
+	score_changed.emit(score) #Hookup to Canvas
+	print("Updated Score:", score)
